262f1c2b85653a12a48c86415ffc48839096cec1
johnmccloskey <john.j.mccloskey76@gmail.com>
2021-09-11 12:07:34 -0500

    Miscellaneous

--- src/main/java/RandomNumberServlet.java
+++ src/main/java/RandomNumberServlet.java
@@ -1,4 +1,4 @@
-import javax.servlet.annotation.WebServlet;
+import gijavax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
